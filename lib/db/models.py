005e3497d81854664108c336bf1008a3f99a4b43
antwoine <antwoinegriggs@outlook.com>
2023-08-15 08:43:37 -0400

    add SCHEMA

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,3 +1,27 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+
 
 Base = declarative_base()
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+
+class Platform(Base):
+    __tablename__ = "platforms"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)\
+    
+class Game(Base):
+    __tablename__ = "games"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+
+
+
--- migrations/env.py
+++ migrations/env.py
@@ -17,7 +17,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models import Base
+from lib.db.models import Base
 target_metadata = Base.metadata
 # target_metadata = None
 
